56a0111577ac0cf1feeaa85c682efd9d056b4f00
Dawei Li <thomashyde23@gmail.com>
2023-08-28 21:28:30 -0700

    added method for calculating max drawdown

--- sdocks/consts.py
+++ sdocks/consts.py
@@ -1,18 +1,24 @@
 import numpy as np
+import pandas as pd
 from numba import jit
+from plotly import graph_objects as go
 from typing import Any,  Dict, List, Optional, Tuple, Union
 
 
 class InvestmentData:
 
+
     def __init__(
         self,
         dates: List[str],
         prices: List[float],
+        ticker: Optional[str]=None,
     ):
         self.dates=dates
         self.prices=np.array(prices, dtype=np.float32)
-        self.dataframe=None
+        self.ticker=ticker
+
+        self._create_data_frame() # creating a dataframe for preserving information
 
 
     @staticmethod
@@ -27,23 +33,57 @@ class InvestmentData:
         return np.log(prices[1:]/prices[:-1])
 
 
+    def _get_shifted_lines(self, feature, function):
+        values=function(self.prices)
+        self.dataframe[feature]=-np.NaN  
+        self.dataframe.loc[1:,feature]=values
+        return values
+
+
     def get_return(self):
-        self.returns=self._get_return(self.prices)
+        self.returns=self._get_shifted_lines('return',self._get_return)
         return self.returns
 
 
     def get_log_return(self):
-        self.log_returns=self._get_log_return(self.prices)
+        self.log_returns=self._get_shifted_lines('log return',self._get_log_return)
         return self.log_returns
     
 
     def _create_data_frame(self):
+        self.dataframe=pd.DataFrame(
+            {
+                'date':pd.to_datetime(self.dates),
+                'price':self.prices,
+            }
+        )
 
+    
+    def _make_line_chart(self, col):
+        fig=go.Figure()
+        fig.add_trace(
+            go.Scatter(
+                x=self.dataframe['date'],
+                y=self.dataframe[col]
+            )
+        )
+        title=f'{col} curve' if not self.ticker else f'{col} curve for {self.ticker}'
+        fig.update_layout(dict(title=title))
 
-    def get_equity_curve(self):
+        fig.show()
 
 
     def get_return_curve(self):
+        self._make_line_chart('return')
         
 
     def get_log_return_curve(self):
+        self._make_line_chart('log return')
+
+
+    def get_price_curve(self):
+        self._make_line_chart('price')
+
+    
+    def get_data(self):
+        return self.dataframe
